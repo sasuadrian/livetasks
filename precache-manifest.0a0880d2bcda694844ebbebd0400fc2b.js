74421ef6ebd912721c1e4d12ce9a5ae7af4a7ecd
adriansasu <sasuadrian12@gmail.com>
2020-04-21 13:00:50 +0300

    Updates

---
+++ precache-manifest.0a0880d2bcda694844ebbebd0400fc2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8bde3babb30dba591e58cbbbd49323",
+    "url": "/livetasks/index.html"
+  },
+  {
+    "revision": "4f0c6bb29104f9514be2",
+    "url": "/livetasks/static/css/main.be0ee844.chunk.css"
+  },
+  {
+    "revision": "3221bc781d42c474763e",
+    "url": "/livetasks/static/js/2.e31b7884.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/livetasks/static/js/2.e31b7884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0c6bb29104f9514be2",
+    "url": "/livetasks/static/js/main.b0d5c859.chunk.js"
+  },
+  {
+    "revision": "7e75391f3f8e8263957a",
+    "url": "/livetasks/static/js/runtime-main.9c5d7d56.js"
+  }
+]);
